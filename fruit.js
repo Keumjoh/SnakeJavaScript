63b9c283fb6b534b0a79d425e1be8145cbb7a800
Kevin Umali <kevinumali0999@gmail.com>
2020-06-04 11:35:35 +0800

    Adding file

---
+++ fruit.js
@@ -0,0 +1,18 @@
+function Fruit() {
+    this.x;
+    this.y;
+
+    this.pickLocation = function()
+    {
+        this.x = (Math.floor(Math.random() *
+        rows - 1) + 1) * scale;
+        this.y = (Math.floor(Math.random() *
+        columns - 1) + 1) * scale;
+    }
+
+    this.drawFruit = function()
+    {
+        ctx.fillStyle = "rgb(200,120,140)";
+        ctx.fillRect(this.x, this.y, scale, scale);
+    }
+}
